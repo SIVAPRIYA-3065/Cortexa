7a87e407731364d57b74873ac00663e0bd03fcff
SIVAPRIYA B <sivapriyabalakrishnan05@gmail.com>
2026-04-01 12:36:59 +0530

    Mail Service

---
+++ Backend/src/main/java/com/dementia /riskassessment/dto/SignupRequest.java
@@ -0,0 +1,89 @@
+package com.dementia.riskassessment.dto;
+
+import jakarta.validation.constraints.*;
+
+public class SignupRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+    
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    
+    @NotNull(message = "Age is required")
+    @Min(value = 0, message = "Age must be at least 0")
+    @Max(value = 120, message = "Age must be at most 120")
+    private Integer age;
+    
+    @NotBlank(message = "Gender is required")
+    private String gender;
+    
+    @NotBlank(message = "Blood group is required")
+    private String bloodGroup;
+    
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+    
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+}
+
